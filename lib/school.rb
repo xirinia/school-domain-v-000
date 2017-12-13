13fbf7e4bd48e623e606a9e537dd8744dddcb1c1
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-13 01:51:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+
+
+  def initialize(name, roster)
+    @name = name
+    @roster = roster
+  end
+end
+
+def add_student(name, grade)
+  @name = name
+  @grade = grade
+end
+
+
+school = School.new("Test School", [])
